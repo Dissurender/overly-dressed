b9a06c3f2d17b485f5284a4078d7c14d47ce580b
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-08-16 17:13:58 -0400

    Added carousel component.

---
+++ src/components/SlideShowCarousel.tsx
@@ -0,0 +1,11 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function SlideShowCarousel() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+}
+
+export default Carousel;
