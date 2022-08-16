4e09488a528fa49ea892efff4d0aa80c4d586047
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-08-16 18:23:15 -0400

    Added sample images with carousel and 3s rotation intervals.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import Navigation from './Navigation';
+import SlideShowCarousel from './SlideShowCarousel';
 
 const App = () => {
     return (
         <div>
             <h1>Hello World!</h1>
             <Navigation />
+            <SlideShowCarousel />
         </div>
     );
 };
--- src/components/SlideShowCarousel.tsx
+++ src/components/SlideShowCarousel.tsx
@@ -1,11 +1,39 @@
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
+
 function SlideShowCarousel() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex: number, e: any) => {
+        setIndex(selectedIndex);
+    };
+
     return (
-        <div>
-            Hello World!
-        </div>
+        <Carousel>
+            <Carousel.Item interval={3000}>
+                <img
+                    className="d-block w-100"
+                    src="https://www.devilinspired.com/image/catalog/banner/2022banner/2-22-7/b6460188a428354484fd5a00aab5613b_%E8%8B%B1%E8%AF%AD.png"
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+                <img
+                    className="d-block w-100"
+                    src="https://www.devilinspired.com/image/catalog/banner/2022banner/2022-6/965c382f1dbf5e80a211aa6085eb626d_%E8%8B%B1%E8%AF%AD.png"
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+                <img
+                    className="d-block w-100"
+                    src="https://www.devilinspired.com/image/catalog/banner/2022banner/2022-6/fe44773729fcc90d86ffbd0863bfb339_%E8%8B%B1%E8%AF%AD.png"
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+        </Carousel>
     );
 }
 
-export default Carousel;
+export default SlideShowCarousel;
