ce28404257a963e529c0f1f78f4e0e65b5f814d3
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-08-17 16:34:10 -0400

    Added ClothingItem component and scss

---
+++ src/components/clothing-item/ClothingItem.tsx
@@ -0,0 +1,13 @@
+import '../../scss/ClothingItem.scss';
+
+const ClothingItem = ({ name, price, imageUrl }: { name: string, price: number, imageUrl: string }) => {
+    <div className='clothing-item'>
+        <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+        <div className='clothing-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+        </div>
+    </div>
+};
+
+export default ClothingItem;
