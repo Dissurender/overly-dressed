805fddc1ff118824a86ca6165e6ac0d53787e484
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-08-18 11:53:06 -0400

    Converted to React18 with ReactDOM.createRoot

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement!).render(
     <App />,
-    document.querySelector("#root")
 );
