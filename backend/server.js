fdf3601241c44301d8a0980dcc485c35d753fc82
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-08-20 19:16:52 -0400

    Added middleware.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ dotenv.config({ path: './config/config.env' });
 const server = express();
 
 const PORT = process.env.PORT || 5000;
-
+server.use(express.urlencoded({ extended: true }))
 
 server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORTls}`));
