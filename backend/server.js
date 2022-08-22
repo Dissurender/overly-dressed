50f6552162b8a348de74c36c8987a9d89df6dd8f
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-08-22 12:48:43 -0400

    Added Dockerfiles and empty docker-compose file.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const { application } = require('express');
 dotenv.config({ path: './config/config.env' });
-
 const server = express();
-
 const PORT = process.env.PORT || 5000;
+
 server.use(express.urlencoded({ extended: true }))
 
-server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORTls}`));
+server.get('/', (req, res) => {
+    res.json({
+        message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s..."
+    })
+});
+
+
+
+server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
