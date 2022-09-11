8083fb3ef32aa1f79b0ef79e08d6a8e38dfd2937
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-09-10 21:41:39 -0400

    Added jest to the test script, implemented tests regarding clothes schema.

--- backend/tests/clothes.test.js
+++ backend/tests/clothes.test.js
@@ -1,21 +1,15 @@
 const mongoose = require("mongoose");
-const clothes = require("./clothes");
+const Clothes = require("../models/clothes.js");
 const db = require("../setup/db");
 
 const clothesData = {
-    id: 'sdfsdf',
+    id: 1,
     name: 'clothesName',
     price: 45.44,
     productDate: Date.now(),
     description: 'Description',
 };
-/*
-id: 
-name: 
-price: 
-productDate: 
-description: 
-*/
+
 beforeAll(async () => {
     await db.setUp();
 });
@@ -27,3 +21,33 @@ afterEach(async () => {
 afterAll(async () => {
     await db.dropDatabase();
 });
+
+describe("Clothes model", () => {
+    it("creates and saves a clothes object successfully", async () => {
+        const validClothesObject = new Clothes(clothesData);
+        const savedClothes = await validClothesObject.save();
+        expect(savedClothes._id).toBeDefined();
+        expect(savedClothes.id).toBe(1);
+        expect(savedClothes.name).toBe('clothesName');
+        expect(savedClothes.price).toBe(45.44);
+        expect(savedClothes.description).toBe('Description');
+    });
+
+    it("create clothes object without required fields should fail", async () => {
+        //should probably be refactored.
+        const clothesWithoutRequiredField = new Clothes({});
+        let err;
+        try {
+            const savedClothesWithoutRequiredField = await clothesWithoutRequiredField.save();
+        } catch (error) {
+            err = error;
+            console.log(err);
+        }
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+        expect(err.errors.name).toBeDefined();
+        expect(err.errors.price).toBeDefined();
+        expect(err.errors.description).toBeDefined();
+        expect(err.errors.id).toBeDefined();
+    })
+});
+
