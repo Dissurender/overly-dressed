29adc5df09f66d118425074123e4ed4c9ff89830
ApplePieAngel <86702974+ApplePieAngel@users.noreply.github.com>
2022-09-12 14:45:02 -0400

    Added guard clauses to getClothes to check for valid queries and returning a non-empty array.

--- backend/tests/routesClothes.test.js
+++ backend/tests/routesClothes.test.js
@@ -1,13 +1,18 @@
 const supertest = require('supertest');
 const app = require('../server.js');
 
-describe("Routes for Clothes", () => {
+//    app.get('/api/clothes/:clothesId', controller.getClothes);
 
-    it("should define the route 'GET' / and call the right handler", async () => {
-        const clothesId = '15';
-        expect(true).toBe(true);
-        //await supertest(app).get(`/duck`).expect(404);
-        //await supertest(app).get(`/api/clothes/${clothesId}`).expect(404);
+describe("Given the routes for the clothes route.", () => {
+    it("should return the Root route", async () => {
+        const { body, statusCode } = await supertest(app).get(`/`);
+        expect(statusCode).toBe(200);
+        expect(body.server_status).toBe("active");
+    });
+    it("should error if the clothesId aren't passed in", async () => {
+        const clothesId = 1;
+        await supertest(app).get(`/api/clothes/?clothesId=${clothesId}`).expect(200);
+        await supertest(app).get(`/api/clothes/${clothesId}`).expect(404);
     });
 });
 
